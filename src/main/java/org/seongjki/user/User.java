6fb3d652396fec19315580e092b13655a08fee2e
seongjunkim <tjdwns5063@naver.com>
2025-01-05 15:04:48 +0900

    fix: User, Channel 양방향 맵핑

--- src/main/java/org/seongjki/user/User.java
+++ src/main/java/org/seongjki/user/User.java
@@ -1,5 +1,9 @@
 package org.seongjki.user;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.seongjki.channel.Channel;
+
 public class User {
 
     private Long id;
@@ -10,6 +14,8 @@ public class User {
 
     private String gender;
 
+    private List<Channel> channels = new ArrayList<>();
+
     public User(Long id, String nickname, String password, String gender) {
         this.id = id;
         this.nickname = nickname;
@@ -17,4 +23,23 @@ public class User {
         this.gender = gender;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public List<Channel> getChannels() {
+        return channels;
+    }
 }
