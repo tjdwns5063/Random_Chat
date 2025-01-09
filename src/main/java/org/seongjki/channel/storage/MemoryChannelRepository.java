a2a27c9801acd4950f904f219484b0533881a56f
seongjunkim <tjdwns5063@naver.com>
2025-01-09 15:07:29 +0900

    feat: 채널 생성 명령어 구현

--- src/main/java/org/seongjki/channel/storage/MemoryChannelRepository.java
+++ src/main/java/org/seongjki/channel/storage/MemoryChannelRepository.java
@@ -1,6 +1,7 @@
 package org.seongjki.channel.storage;
 
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 import org.seongjki.channel.Channel;
 
 public class MemoryChannelRepository implements ChannelRepository {
@@ -29,5 +30,10 @@ public class MemoryChannelRepository implements ChannelRepository {
     @Override
     public Optional<Channel> findByName(String name) {
         return channels.stream().filter(ch -> Objects.equals(ch.getName(), name)).findFirst();
+    }
+
+    @Override
+    public boolean isPresentByName(String name) {
+        return channels.stream().anyMatch(ch -> StringUtils.equals(name, ch.getName()));
     }
 }
